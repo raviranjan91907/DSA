52f27fa53631a45a86098b34dfdf00e011f762e0
raviranjan91907 <raviranjan91907@gmail.com>
2026-04-17 23:04:03 +0530

    list

--- STL/listSTL.cpp
+++ STL/listSTL.cpp
@@ -1,3 +1,17 @@
+/*
+Doubly Linked List
+Each node has:
+Data
+Pointer to next node
+Pointer to previous node
+Non-contiguous memory
+Elements are not stored in continuous memory like arrays or vectors.
+Fast insertion & deletion
+Insertion/deletion anywhere → O(1) (if position is known)
+No random access
+Cannot use l[2] like vector
+Access is sequential → O(n)
+*/
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
